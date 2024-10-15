36698ae226e79f38176f9f288386b3c8053ae11e
nikunj63 <nikunjs6324@gmail.com>
2024-10-15 11:04:34 +0530

    update

---
+++ note_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/firebase_options.dart';
+import 'package:flutter_crud/pages/home.dart';
+
+void main ()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp( const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
---
+++ note_app/lib/pages/employee.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/services/database.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:random_string/random_string.dart';
+
+class Employee extends StatefulWidget {
+  const Employee({super.key});
+
+  @override
+  State<Employee> createState() => _EmployeeState();
+}
+
+class _EmployeeState extends State<Employee> {
+  TextEditingController namecontoller= new TextEditingController();
+  TextEditingController agecontoller= new TextEditingController();
+  TextEditingController locationcontoller= new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Employee",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 25,
+                fontWeight:FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Form",
+              style: TextStyle(
+                color: Colors.orange,
+                fontSize: 25,
+                fontWeight:FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        margin: const EdgeInsets.only(left: 20.0,top: 30.0,right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+           const Text(
+              "Name",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+           const SizedBox(height: 10.0,),
+            Container(
+              padding: const EdgeInsets.only(left: 10.0),
+              decoration: BoxDecoration(
+                border: Border.all(),borderRadius: BorderRadius.circular(10)
+              ),
+              child: TextField(
+                controller: namecontoller ,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                ),
+              ),
+              
+            ),
+           const SizedBox(height: 20,),
+           const Text(
+              "Age",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+           const SizedBox(height: 10.0,),
+            Container(
+              padding:const EdgeInsets.only(left: 10.0),
+              decoration: BoxDecoration(
+                border: Border.all(),borderRadius: BorderRadius.circular(10)
+              ),
+              child: TextField(
+                controller: agecontoller,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+           const SizedBox(height: 20,),
+           const Text(
+              "Location",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+           const SizedBox(height: 10.0,),
+            Container(
+              padding: const EdgeInsets.only(left: 10.0),
+              decoration: BoxDecoration(
+                border: Border.all(),borderRadius: BorderRadius.circular(10)
+              ),
+              child: TextField(
+                controller: locationcontoller,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+           const SizedBox(height: 30.0,),
+            Center(
+              child: ElevatedButton(
+                onPressed: () async{
+                  String ID = randomAlphaNumeric(10);
+                  Map<String, dynamic> employeeInfoMap={
+                    "ID":ID,
+                    "Name":namecontoller.text,
+                    "Age":agecontoller.text,
+                    "Location":locationcontoller.text,
+                  };
+                  await Database().addEmployeeDetails(employeeInfoMap,ID).then((value){
+                    Fluttertoast.showToast(
+        msg: "Employee detail has been uploaded sucessfully",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
+                  });
+                }, 
+                child: const Text(
+                  "Add",style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
